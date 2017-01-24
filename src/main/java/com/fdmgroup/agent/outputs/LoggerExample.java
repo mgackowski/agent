bbd630dac7107b98a17837c7f77f8a0d8096db99
mgackowski <mgackowski@192.168.1.25>
2017-01-24 19:27:09 +0000

    Simplified config file; file writing disabled

--- src/main/java/com/fdmgroup/agent/outputs/LoggerExample.java
+++ src/main/java/com/fdmgroup/agent/outputs/LoggerExample.java
@@ -5,9 +5,9 @@ import org.apache.logging.log4j.Logger;
 
 public class LoggerExample {
 	
-	static Logger log = LogManager.getLogger(LoggerExample.class.getName());
+	static Logger log = LogManager.getLogger();
 	
 	public static void testLogs() {
-		log.debug("Hi, this is a Debug message");
+		log.debug("Hi, this is a Debug message I can understand");
 	}
 }
