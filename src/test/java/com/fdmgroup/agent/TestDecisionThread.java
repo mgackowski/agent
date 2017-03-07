b82cb56eed40013f7330c2d0416568cb72fa5d8d
mgackowski <mwgackowski@gmail.com>
2017-03-07 16:11:17 +0000

    Started writing Mockito test in TestDecisionThread

--- src/test/java/com/fdmgroup/agent/TestDecisionThread.java
+++ src/test/java/com/fdmgroup/agent/TestDecisionThread.java
@@ -1,13 +1,50 @@
 package com.fdmgroup.agent;
 
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import com.fdmgroup.agent.actions.BasicAction;
+import com.fdmgroup.agent.agents.Agent;
+import com.fdmgroup.agent.objects.BasicObject;
+import com.fdmgroup.agent.objects.UseableObject;
+import com.fdmgroup.agent.threads.DecisionThread;
+
 public class TestDecisionThread {
+	
+	DecisionThread testDecisionThread;
+	Agent testAgent;
+	BasicObject testObject;
+	BasicAction testAction;
+	List<UseableObject> testList;
+	
+	
+	
+	@Before
+	public void setUpDecisionThreadTest() {
+		
+		testAgent = mock(Agent.class);
+		testObject = mock(BasicObject.class);
+		testAction = mock(BasicAction.class);
+		testList = new ArrayList<UseableObject>();
+		//testObject
+		testList.add(testObject);
+		
+		testDecisionThread = new DecisionThread(testAgent, testList);
+		
+	}
 	
 	@Ignore
 	@Test
 	public void TestDecisionThread_QueryEnvironmentForPossibilities_HigherDeltaMeansHigherScore() {
+		List actionList = new ArrayList();
+		actionList.add(null);
+		when(testObject.advertiseActions()).thenReturn(null);
 		// At need_1 = 80f, action N1+15f is preferable to N1+5f
 	}
 	
